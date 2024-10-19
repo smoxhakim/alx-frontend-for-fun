8fc8cb040d33a328019cc272fe3c1351a21da72b
hakimsmox <abdelhakimsmox@gmail.com>
2024-10-19 19:58:08 +0100

    markdown2html.py

---
+++ markdown2html.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Markdown is awesome! """
+
+import sys
+import os
+
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.exit(1)
+
+    markdown_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    if not os.path.isfile(markdown_file):
+        print(f"Missing {markdown_file}", file=sys.stderr)
+        sys.exit(1)
+
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
